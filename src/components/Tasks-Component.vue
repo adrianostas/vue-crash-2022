7cb8e3f5231629aa0f53a0692036f201a078c345
adrianostas <adrianostas87@gmail.com>
2023-01-01 16:17:23 +0200

    vue course

---
+++ src/components/Tasks-Component.vue
@@ -0,0 +1,27 @@
+<template>
+  <div v-for="task in tasks" :key="task.id">
+    <Task @toggle-task="$emit('toggle-task', task.id)" @delete-task="$emit('delete-task', task.id)" :task="task"/>
+
+  </div>
+
+</template>
+
+<script>
+
+import Task from './Task-Component.vue'
+
+export default {
+  name: "Tasks-Component",
+  props: {
+    tasks: Array
+  },
+  components: {
+    Task
+  },
+  emits: ['delete-task', 'toggle-task']
+}
+</script>
+
+<style scoped>
+
+</style>
